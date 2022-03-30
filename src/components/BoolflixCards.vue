041485a3219a15ae4fd38914174774b490a5a0fe
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-30 15:25:50 +0200

    -ho fatto il controllo sulle lingue principali-

--- src/components/BoolflixCards.vue
+++ src/components/BoolflixCards.vue
@@ -2,9 +2,8 @@
   <div class="cards">
     <h1>{{ CardData.original_title ? CardData.original_title : CardData.original_name }}</h1>
     <h2>{{ CardData.title ? CardData.title : CardData.name }}</h2>
-    <div class="lang">
-      <lang-flag :iso="CardData.original_language" />
-    </div>
+      <lang-flag v-if="checkLangFlag(CardData.original_language)" :iso="CardData.original_language" class="lang" />
+      <div v-else>{{ CardData.original_language }}</div>
     <div class="rating">{{ CardData.vote_average }}</div>
   </div>
 </template>
@@ -15,6 +14,27 @@ export default {
   name: 'MoviesCards',
   props: {
     CardData: Object
+  },
+  data () {
+    return {
+      arrLang: [
+        'en',
+        'fr',
+        'de',
+        'it',
+        'ja',
+        'pt',
+        'ru',
+        'es'
+      ]
+    }
+  },
+  methods: {
+    checkLangFlag (flag) {
+      if (this.arrLang.includes(flag)) {
+        return this.arrLang
+      }
+    }
   }
 }
 </script>
